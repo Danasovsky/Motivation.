4555e16f32499bc24aa86d2f8b4239cec15289a7
Eline Danasovsky <75419773+Danasovsky@users.noreply.github.com>
2020-12-08 20:11:20 +0100

    Update animate.js

--- animate.js
+++ animate.js
@@ -55,7 +55,7 @@ window.onload = function() {
 // inject CSS
         var css = document.createElement("style");
         css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.05em solid #fff}";
         document.body.appendChild(css);
     };   
 
